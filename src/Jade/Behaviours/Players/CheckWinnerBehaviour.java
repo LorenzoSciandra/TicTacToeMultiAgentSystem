6e9a3444d3f76fd25af4de89b7ddc095f406ab69
Federico "EvilScript" Torrielli <evilscript@protonmail.com>
2022-07-17 18:12:29 +0200

    quasi finito swag 2

---
+++ src/Jade/Behaviours/Players/CheckWinnerBehaviour.java
@@ -0,0 +1,60 @@
+package Jade.Behaviours.Players;
+
+import Jade.Grid;
+import Jade.Agents.Player;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class CheckWinnerBehaviour extends CyclicBehaviour {
+
+    @Override
+    public void action() {
+        MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM_IF);
+        ACLMessage msg = myAgent.receive(mt);
+        if (msg != null) {
+            System.out.println(getAgent().getName() + " " + msg.getContent());
+            switch (msg.getContent()) {
+                case "WIN":
+                    System.out.println("Agent " + getAgent().getAID().getName() + " ha vinto.");
+                    ((Player) getAgent()).setGrid(new Grid());
+
+                    if (((Player) getAgent()).getRound() < ((Player) getAgent()).getTotalRounds()) {
+                        System.out.println("HO VINTO E ASPETTO IL PROSSIMO ROUND " + getAgent().getName());
+                        ((Player) getAgent()).setRound(((Player) getAgent()).getTotalRounds() + 1);
+                        getAgent().addBehaviour(new ReceiveOpponentBehaviour(((Player) getAgent()).getStupid()));
+                    }
+                    break;
+                case "LOSE":
+                    System.out.println("Agent " + getAgent().getAID().getName() + " ha perso.");
+                    ((Player) getAgent()).setRound(((Player) getAgent()).getTotalRounds() + 1);
+                    break;
+                case "TIE": // TIE
+                    System.out.println("PAREGGIO");
+                    ((Player) getAgent()).setGrid(new Grid());
+                    if (((Player) getAgent()).getStart()) {
+                        getAgent().doWait(1000);
+                        //((Player) getAgent()).setStart(false);
+                        //((Player) getAgent()).setSymbol("O");
+                        if (((Player) getAgent()).getStupid()) {
+                            // aspettiamo che anche l'altro player abbia resettato il tutto
+                            getAgent().addBehaviour(new PlayBehaviour());
+                        } else {
+                            getAgent().addBehaviour(new IntelligentPlayBehaviour());
+                        }
+                    } else {
+                        //((Player) getAgent()).setStart(true);
+                        //((Player) getAgent()).setSymbol("X");
+                        getAgent().addBehaviour(new ReceiveMessageBehaviour());
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+        } else {
+            block();
+        }
+    }
+
+}
---
+++ src/Jade/Messages/EndMessage.java
@@ -0,0 +1,7 @@
+package Jade.Messages;
+
+import jade.util.leap.Serializable;
+
+public enum EndMessage implements Serializable {
+    WIN, LOSE, TIE
+}
